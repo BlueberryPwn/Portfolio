55a4df6d702eb277eb38929accaa88629da36e70
Nshoan Abdlwafa <nshoan99@gmail.com>
2025-02-13 19:24:49 +0100

    Added responsive hamburger menu to the navbar and updated CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,52 @@
 import "../styles/Navbar.css";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import CloseIcon from "@mui/icons-material/Close";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="links">
-      <Link to="hero" smooth>
-        Home
-      </Link>
-      <Link to="about" smooth>
-        About
-      </Link>
-      <Link to="skills" smooth>
-        Skills
-      </Link>
-      <Link to="projects" smooth>
-        Projects
-      </Link>
-      <Link to="contact" smooth>
-        Contact
-      </Link>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  return (
+    <nav className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="navbar-toggle">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          {expandNavbar ? <CloseIcon /> : <ReorderIcon />}
+        </button>
+      </div>
+      <ul className="navbar-links">
+        <li className="navbar-link">
+          <Link to="hero" smooth>
+            Home
+          </Link>
+        </li>
+        <li className="navbar-link">
+          <Link to="about" smooth>
+            About
+          </Link>
+        </li>
+        <li className="navbar-link">
+          <Link to="skills" smooth>
+            Skills
+          </Link>
+        </li>
+        <li className="navbar-link">
+          <Link to="projects" smooth>
+            Projects
+          </Link>
+        </li>
+        <li className="navbar-link">
+          <Link to="contact" smooth>
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
