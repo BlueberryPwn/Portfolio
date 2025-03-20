0a51fce11532e9e475e1870b7c7e92476031dcde
Nshoan Abdlwafa <nshoan99@gmail.com>
2025-03-20 16:46:27 +0100

    Added content to the hero section and updated some CSS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
 import "../styles/Hero.css";
+import IconButton from "@mui/material/IconButton";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Hero = () => (
   <section id="hero">
-    <p className="hero-description">This is the hero section.</p>
+    <h2 className="hero-title">Hi, I'm Nshoan.</h2>
+    <p className="hero-description">I'm a fullstack developer.</p>
+    <div className="icon-button">
+      <IconButton
+        aria-label="visit"
+        onClick={() => window.open("https://github.com/BlueberryPwn")}
+      >
+        <GitHubIcon />
+      </IconButton>
+
+      <IconButton
+        aria-label="visit"
+        onClick={() =>
+          window.open("https://www.linkedin.com/in/nshoan-a-853878208/")
+        }
+      >
+        <LinkedInIcon />
+      </IconButton>
+    </div>
   </section>
 );
 
