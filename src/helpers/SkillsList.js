c9289482f18298641919b3181273250deed6760d
Nshoan Abdlwafa <nshoan99@gmail.com>
2025-03-21 11:21:29 +0100

    Added assets and helpers to be used for skills & socials

---
+++ src/helpers/SkillsList.js
@@ -0,0 +1,75 @@
+import Azure from "../assets/Azure.svg";
+import CS from "../assets/CS.svg";
+import CSS from "../assets/CSS.svg";
+import DotNet from "../assets/DotNet.svg";
+import Git from "../assets/Git.svg";
+import HTML from "../assets/HTML.svg";
+import JavaScript from "../assets/JavaScript.svg";
+import PostgreSQL from "../assets/PostgreSQL.svg";
+import React from "../assets/React.svg";
+import TailwindCSS from "../assets/TailwindCSS.svg";
+import VisualStudio from "../assets/VisualStudio.svg";
+import VSCode from "../assets/VSCode.svg";
+
+export const SkillsList = [
+  {
+    id: 0,
+    name: "Azure",
+    image: Azure,
+  },
+  {
+    id: 1,
+    name: "C#",
+    image: CS,
+  },
+  {
+    id: 2,
+    name: "CSS",
+    image: CSS,
+  },
+  {
+    id: 3,
+    name: ".NET",
+    image: DotNet,
+  },
+  {
+    id: 4,
+    name: "Git",
+    image: Git,
+  },
+  {
+    id: 5,
+    name: "HTML",
+    image: HTML,
+  },
+  {
+    id: 6,
+    name: "JavaScript",
+    image: JavaScript,
+  },
+  {
+    id: 7,
+    name: "PostgreSQL",
+    image: PostgreSQL,
+  },
+  {
+    id: 8,
+    name: "React.js",
+    image: React,
+  },
+  {
+    id: 9,
+    name: "Tailwind CSS",
+    image: TailwindCSS,
+  },
+  {
+    id: 10,
+    name: "Visual Studio",
+    image: VisualStudio,
+  },
+  {
+    id: 11,
+    name: "VS Code",
+    image: VSCode,
+  },
+];
---
+++ src/helpers/SocialsList.js
@@ -0,0 +1,17 @@
+import GitHub from "../assets/GitHub.svg";
+import LinkedIn from "../assets/LinkedIn.svg";
+
+export const SocialsList = [
+  {
+    id: 0,
+    name: "GitHub",
+    image: GitHub,
+    link: "https://github.com/BlueberryPwn",
+  },
+  {
+    id: 1,
+    name: "LinkedIn",
+    image: LinkedIn,
+    link: "https://www.linkedin.com/in/nshoan-a-853878208/",
+  },
+];
